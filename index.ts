0bec281b6394a752a06488a3483b748297a6ba85
湫曗 <autumnsilverleaves@gmail.com>
2021-03-10 01:07:50 +0800

    invertTree

--- index.ts
+++ index.ts
@@ -4,8 +4,24 @@ interface Node {
 }
 
 function count(node: Node | null): number {
+  //   if (node === null) {
+  //     return 0;
+  //   } else {
+  //     return 1 + count(node.left) + count(node.right);
+  //   }
+
+  return node ? 1 + count(node.left) + count(node.right) : 0;
+}
+
+function invertTree(node: Node | null): void {
   if (node === null) {
-    return 0;
+    return;
+  } else {
+    const temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+
+    invertTree(node.left);
+    invertTree(node.right);
   }
-  return 1 + count(node.left) + count(node.right);
 }
